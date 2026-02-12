e697f06a743bd4b064038680e01a2c9c127d367c
wblee <wblee@chunjae.co.kr>
2026-02-12 01:48:47 +0000

    delete unnessesary folders and update README.md

--- build/lib/pickt/lightning_modules/__init__.py
+++
@@ -1,7 +0,0 @@
-# coding : utf-8
-# edit : 
-# - author : wblee
-# - date : 2025-05-21
-
-from .model_module import ModelModule
-from .data_module import DataModule
--- build/lib/pickt/utils/__init__.py
+++
@@ -1,9 +0,0 @@
-# coding : utf-8
-# edit : 
-# - author : wblee
-# - date : 2025-04-28
-
-from .pickt_logger import pickt_logger
-from .reader import load_data, save_json, create_folder_if_not_exists
-from .decorator import measure_execution_time
-from .callback import PredictionCollector
